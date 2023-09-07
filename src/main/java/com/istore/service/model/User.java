3f3d7ff23dd2436077664d7ba9d15b6063bd8426
Sainath Bottupally <bottupallysainath@gmail.com>
2023-09-07 13:31:54 +0530

    Enable cors

--- src/main/java/com/istore/service/model/User.java
+++ src/main/java/com/istore/service/model/User.java
@@ -2,12 +2,14 @@ package com.istore.service.model;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 @Document("users")
 @AllArgsConstructor()
 @Getter()
+@NoArgsConstructor
 public class User {
     @Id()
     private String id;
